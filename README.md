75b6e53e6e56c148473a5f20a3e26f42a98bfcff
Richard Audette <richard_audette@yahoo.ca>
2024-05-16 08:42:10 -0400

    First check-in

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # meshtastic_discord_bridge
+
 A Discord bot which bridges discussions between a Discord channel and a Meshtastic mesh through a locally connected radio
+
+## Requirements
+
+- Python
+- A supported Meshtastic radio connected via USB 
+- Discord key and a channel
+
+## Installation and Startup
+
+Get a Discord Bot account and invite the bot to a server.  [Instructions](https://discordpy.readthedocs.io/en/stable/discord.html)
+
+Fill in the values for your environment in sampledotenvfile, and rename to .env 
+
+```
+python3 -m pip install -r requirements.txt
+python meshtastic_discord_bridge.py
+```
+
+## Usage
+
+You can now interact with Meshtastic through Discord.
+
+```
+$sendprimary <message> sends a message up to 225 characters to the the primary channel
+$send nodenum=########### <message> sends a message up to 225 characters to nodenum ###########
+$activenodes will list all nodes seen in the last 15 minutes
+```
+
+## Screenshot
+
+![Interacting with Meshtastic through Discord](/DiscordScreenshot.png)
+
---
+++ meshtastic_discord_bridge.py
@@ -0,0 +1,147 @@
+import discord
+import asyncio
+import os
+import sys
+from dotenv import load_dotenv
+from pubsub import pub
+import meshtastic
+import meshtastic.serial_interface
+import queue
+import time
+from datetime import datetime
+
+load_dotenv()
+token = os.getenv("DISCORD_TOKEN")
+channel_id = int(os.getenv("DISCORD_CHANNEL_ID"))
+
+meshtodiscord = queue.Queue()
+discordtomesh = queue.Queue()
+nodelistq = queue.Queue()
+
+def onConnectionMesh(interface, topic=pub.AUTO_TOPIC):  
+    """called when we (re)connect to the meshtastic radio"""
+    print(interface.myInfo)
+
+def onReceiveMesh(packet, interface):  
+    """called when a packet arrives from mesh"""
+    try: 
+        if packet['decoded']['portnum']=='TEXT_MESSAGE_APP': #only interest in text packets for now
+            meshtodiscord.put("Node "+packet['fromId']+" writes to node "+packet['toId']+ " this message: " +packet['decoded']['text'])
+    except KeyError as e: #catch empty packet
+        pass
+
+class MyClient(discord.Client):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    async def setup_hook(self) -> None:
+        # create the background task and run it in the background
+        self.bg_task = self.loop.create_task(self.my_background_task())
+
+    async def on_ready(self):
+        print(f'Logged in as {self.user} (ID: {self.user.id})')
+        print('------')
+
+    async def on_connection(self):  # pylint: disable=unused-argument
+        # nothing to do here
+        return
+
+
+    async def on_message(self, message):
+        if message.author.id == self.user.id:
+            return
+
+        if message.content.startswith('$help'):
+            helpmessage="Meshtastic Discord Bridge is up.  Command list:\n"\
+                "$sendprimary <message> sends a message up to 225 characters to the the primary channel\n"\
+                "$send nodenum=########### <message> sends a message up to 225 characters to nodenum ###########\n"\
+                "$activenodes will list all nodes seen in the last 15 minutes"
+            await message.channel.send(helpmessage)
+
+        if message.content.startswith('$sendprimary'):
+            tempmessage=str(message.content)
+            tempmessage=tempmessage[tempmessage.find(' ')+1:225] #could be 228
+            await message.channel.send('Sending the following message to the primary channel:\n'+tempmessage)
+            discordtomesh.put(tempmessage)
+
+        if message.content.startswith('$send nodenum='):
+            tempmessage=str(message.content)
+            nodenumstr=tempmessage[14:tempmessage.find(' ',14)+1]
+            tempmessage=tempmessage[tempmessage.find(' ',14)+1:225] #could be 228
+            try:
+                nodenum=int(nodenumstr)
+                await message.channel.send('Sending the following message:\n'+tempmessage+'\nto nodenum:\n'+str(nodenum))
+                discordtomesh.put("nodenum="+str(nodenum)+ " "+tempmessage)
+            except:
+                await message.channel.send('Could not send message')
+                 
+ 
+
+        if message.content.startswith('$activenodes'):
+            nodelistq.put("just pop a message on this queue so we know to send nodelist to discord")
+
+
+    async def my_background_task(self):
+        await self.wait_until_ready()
+        counter = 0
+        nodelist=""
+        channel = self.get_channel(channel_id) 
+        pub.subscribe(onReceiveMesh, "meshtastic.receive")
+        pub.subscribe(onConnectionMesh, "meshtastic.connection.established")
+        try:
+            iface =  meshtastic.serial_interface.SerialInterface()
+        except Exception as ex:
+            print(f"Error: Could not connect {ex}")
+            sys.exit(1)
+        while not self.is_closed():
+            counter += 1
+            if (counter%12==1):
+                #approx 1 minute (every 12th call, call every 5 seconds), refresh node list
+                nodelist="Nodes seen in the last 15 minutes\n"
+                nodes=iface.nodes
+                for node in nodes:
+                    try:
+                            id = nodes[node]['user']['id']
+                            num = nodes[node]['num']
+                            longname = nodes[node]['user']['longName']
+                            if "hopsAway" in nodes[node]:
+                                hopsaway = nodes[node]['hopsAway']
+                            else:
+                                hopsaway=0
+                            snr = nodes[node]['snr']
+                            ts=int(nodes[node]['lastHeard'])
+                            timestr = datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+                            if ts>time.time()-(15*60):
+                                nodelist=nodelist+"\nid:"+id + ", num:"+str(num)+", longname:" + longname + ", hops:" + str(hopsaway) + ", snr:"+str(snr)+", lastheardutc:"+timestr 
+                    except KeyError:
+                        pass
+            try:
+                meshmessage=meshtodiscord.get_nowait()
+                await channel.send(meshmessage)
+                meshtodiscord.task_done()
+            except queue.Empty:
+                pass
+            try:
+                meshmessage=discordtomesh.get_nowait()
+                if meshmessage.startswith('nodenum='):
+                    nodenum=int(meshmessage[8:meshmessage.find(' ')])
+                    iface.sendText(meshmessage[meshmessage.find(' ')+1:],destinationId=nodenum)
+                else:    
+                    iface.sendText(meshmessage)
+                discordtomesh.task_done()
+            except: #lets pass on both the empty queue and the int conversion
+                pass
+            try:
+                nodelistq.get_nowait()
+                #if there's any item on this queue, we'll send the nodelist
+                await channel.send(nodelist)
+                nodelistq.task_done()
+            except queue.Empty:
+                pass
+            await asyncio.sleep(5) 
+        
+intents=discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+client.run(token)
